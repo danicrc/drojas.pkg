100e649e2422f9bf400129692e0190fb974da362
Dani Rojas <daniela.rojas@selu.edu>
2023-11-09 20:03:04 +0000

    updated select fuction

--- Rojas.pkg.Rcheck/00_pkg_src/Rojas.pkg/R/Select.R
+++ Rojas.pkg.Rcheck/00_pkg_src/Rojas.pkg/R/Select.R
@@ -1,17 +1,16 @@
 #' This function is for select specific data 
 #' 
-#' Use for select the VTG, E2, Testosterone and BHB data.
-#' Returns concentration values of this hormones. 
-#' 
-#' @param VTG concentration of an hormone
-#' @param E2 concentration of an hormone
-#' @param Testosterone concentration of an hormone
-#' @param BHB concentration of an hormone
-#' @return columns The concentration of the hormones (numeric)
+#' Use for select the hormone concentration columns that arein data set.
+#' Returns concentration values of the selected hormones.
+#'  
+#' @param data in the date is expecting that each hormone is in each column
+#' @param column_3 concentration of an hormone
+#' @param column_4 concentration of an hormone
+#' @param column_5 concentration of an hormone
+#' @param column_6 concentration of an hormone
+#' @return columns the concentration of the hormones (numeric)
 #' 
 #'@export
-
-
 
 
 
@@ -21,4 +20,4 @@ function_DR <- function(data, column_3, column_4, column_5, column_6){
   return(column_select)
 }
 
-function_DR(Data_RojasC, VTG, E2, Testosterone, BHB)
+#function_DR(Data_RojasC, VTG, E2, Testosterone, BHB)
