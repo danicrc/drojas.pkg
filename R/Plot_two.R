a6f470a7e0c7cf08a8ddbf731bd2106f20ca37ab
Dani Rojas <daniela.rojas@selu.edu>
2023-11-21 21:26:35 +0000

    turtle

--- R/Plot_two.R
+++ R/Plot_two.R
@@ -3,19 +3,20 @@
 #' Use for plot hormones concentration that are in a the data set.
 #' Returns plot of the hormone concentration of the data selected. 
 #' 
-#'@param data must be a data set that contains numeric data for plot the aes. 
-#'@param column_1 include here the data that you want to use for the x aes. In this case you will be using the hormone concentrations.
-#'@param column_2 include here the data that you want to use for the y aes. In this case you will be using the hormone concentrations.
-#'@return the graph with the data that that you selected (numeric).
+#' @param data must be a data set that contains numeric data for plot the aes. 
+#' @param column_1 include here the data that you want to use for the x aes. In this case you will be using the hormone concentrations.
+#' @param column_2 include here the data that you want to use for the y aes. In this case you will be using the hormone concentrations.
+#' @param column_3 include here character data for color. In this case you will be using Note.
+#' @return the graph with the data that that you selected (numeric).
 #' 
-#'@export
+#' @export
 #'
 #'
 
 
-Plot_two <- function(data, column_1, column_2) {
+Plot_two <- function(data, column_1, column_2, column_3) {
   if (is.numeric(data[[column_1]])) {
-    graph <- ggplot(data, aes_string(x = column_1, y = column_2)) + geom_jitter(color = "darkgreen", stat = "identity")
+    graph <- ggplot(data, aes_string(x = column_1, y = column_2, color = column_3 )) + geom_jitter(stat = "identity")
     return(graph)
   } else {
     print("turtle")
--- README.md
+++ README.md
@@ -2,15 +2,23 @@
 
 ## An R package for processing sea turtle hormones data
 
-Users can use this package can use to understand the relationship between hormone concentration during the sea turtle nesting process or preparing for the nesting process.
+Users can use this package can use to understand the relationship between hormone concentration during the sea turtle gravids and non-gravids, before the nesting season. 
 
 #Overview:
 
+
 #functions
+
 
 #nstallation (including my data)
 
+#install.packages ("googlesheets4")
+library(googlesheets4)
+googlesheets4::gs4_deauth()
+hormones <- read_sheet("https://docs.google.com/spreadsheets/d/1X8VwD7pLS437uzxXK2OaZDiklGRgoYlYfCu3BsmlO5s/edit?usp=sharing")
 
-#Usage para que usar el paquete y como hacerlo
+
+#Usage para que usar el paquete y como hacerlo-
+For this package you will need to download the data spredsheet called "Data_RojasC_1_". Tha data set is include there. You also can use this pacakage to compare similar data  
 
 include each funtion in this package
